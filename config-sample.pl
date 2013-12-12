32841a650204f8e1f80186e49e9f3404ab830fa1
HIROSE Masaaki <hirose31@gmail.com>
2013-12-12 15:53:03 +0900

    Initial release

---
+++ config-sample.pl
@@ -0,0 +1,13 @@
++{
+    # aws elb describe-load-balancers | jq '.LoadBalancerDescriptions[].DNSName' | awk '{print $0 ","}' | sort
+    dnsname => [
+        'XXX.us-west-1.elb.amazonaws.com',
+        'YYY.us-west-1.elb.amazonaws.com',
+    ],
+    notify => {
+        ikachan => {
+            url     => 'http://127.0.0.1:4979',
+            channel => '#fixme',
+        },
+    },
+};
